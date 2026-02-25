a98c2cace2002c98da30e5ade3326b816f1c0381
dawsonfbroadbent <dawsonfbroadbent@gmail.com>
2026-02-25 10:11:14 -0700

    changes

---
+++ Mission08_Team0105/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mission08_Team0105.Models;
+
+public partial class Task
+{
+    public int TaskId { get; set; }
+
+    public string TaskName { get; set; } = null!;
+
+    public DateTime? DueDate { get; set; }
+
+    public int Quadrant { get; set; }
+
+    public string? Category { get; set; }
+
+    public int Completed { get; set; }
+}
---
+++ Mission08_Team0105/Models/TasksContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Team0105.Models;
+
+public partial class TasksContext : DbContext
+{
+    public TasksContext()
+    {
+    }
+
+    public TasksContext(DbContextOptions<TasksContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=tasks.sqlite");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.ToTable("tasks");
+
+            entity.Property(e => e.TaskId).HasColumnName("task_id");
+            entity.Property(e => e.Category).HasColumnName("category");
+            entity.Property(e => e.Completed).HasColumnName("completed");
+            entity.Property(e => e.DueDate)
+                .HasColumnType("DATE")
+                .HasColumnName("due_date");
+            entity.Property(e => e.Quadrant).HasColumnName("quadrant");
+            entity.Property(e => e.TaskName).HasColumnName("task_name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
